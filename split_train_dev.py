1873c8f85d9481076bed450fa1ca5bf956501e20
Mickus Timothee <mickus@lx2-400-26939.ad.helsinki.fi>
2022-12-20 15:57:24 +0200

    create dev/train

--- split_train_dev.py
+++ split_train_dev.py
@@ -11,16 +11,18 @@ parser = argparse.ArgumentParser()
 parser.add_argument('infile', type=open_json)
 parser.add_argument('outdev', type=pathlib.Path)
 parser.add_argument('outtrain', type=pathlib.Path)
-parser.add_argument('--dev-size', type=int)
+parser.add_argument('--dev-size', type=int, default=1_000)
 args = parser.parse_args()
 
-dev_indices = set(random.choices(range(len(args.infile)), args.dev_size))
+dev_indices = set(random.sample(range(len(args.infile)), k=args.dev_size))
 outdev_items, outtrain_items = [], []
 for idx, item in enumerate(args.infile):
     if idx in dev_indices:
         outdev_items.append(item)
     else:
         outtrain_items.append(item)
+
+print('sizes:', len(args.infile), '=>', len(outdev_items), '/', len(outtrain_items))
 
 with open(args.outtrain, 'w') as fh:
     json.dump(outtrain_items, fh)
